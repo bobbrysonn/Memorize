75a87bc14ba3e39fbc21f570580a79de02789a0c
Bob Moriasi <official.bobmoriasi@gmail.com>
2023-08-29 14:15:45 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Bob Moriasi on 8/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    var body: some View
+    {
+        HStack
+        {
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View
+{
+    var body: some View
+    {
+        ZStack
+        {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 10)
+                .strokeBorder(lineWidth: 2)
+            VStack
+            {
+                Text("Hello world")
+                Text("👻")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bob Moriasi on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
